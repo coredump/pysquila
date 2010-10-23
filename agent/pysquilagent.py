77c6e2a41b841424112bac48e4a18a19f91670e3
José de Paula Eufrásio Júnior <jose.junior@gmail.com>
2010-10-23 02:29:50 -0200

    Simple config

---
+++ agent/pysquilagent.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys
+import ConfigParser
+import pysquila_agent
+
+config = ConfigParser.ConfigParser()
+if len(sys.argv) > 1:
+    # config file on the command line?
+    try:
+        config.readfp(open(sys.argv[1]))
+    except:
+        print "Problem reading the config file"
+        sys.exit(3)
+else:
+    # try other places
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    local_file = os.path.join(current_dir, 'pysquilagent.cfg')
+    result = config.read(['/etc/pysquilagent.cfg', local_file])
+    if len(result) == 0:
+        print "No config file. Unable to proceed."
+        sys.exit(3)
+
+access_log = config.get('Logs', 'AccessLog')
